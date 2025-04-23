7fd59f5d2602dbdf32862825c84b62e2a1ac2567
Thomas-Ulrich <ulrich@geophysik.uni-muenchen.de>
2025-04-23 10:19:57 +0200

    fix gmsh requiring mesa

--- dynworkflow/generate_mesh.py
+++ dynworkflow/generate_mesh.py
@@ -14,6 +14,10 @@ def generate(h_domain, h_fault, interactive):
     # z0, z1 = -length_added, 5e3
 
     gmsh.initialize()
+    if not interactive:
+        # Avoid GUI display-related calls
+        gmsh.option.setNumber("General.Terminal", 1)
+
     gmsh.model.add("finite-fault")
 
     # We can log all messages for further processing with:
