da228f9e76c953cf9382ea5b2358a7b83f81ecf8
Thomas-Ulrich <ulrich@geophysik.uni-muenchen.de>
2025-11-12 13:38:02 +0100

    remove some duplicated code

---
+++ scripts/common/generate_syn.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+# Common workflow for generating synthetic data
+
+set -euo pipefail
+
+ulimit -Ss 2097152
+
+export MP_SINGLE_THREAD=no
+unset KMP_AFFINITY
+
+output_dir=extracted_output
+script_dir=../rapid-earthquake-dynamics/
+
+if [ -f $output_dir/dyn-kinmod_compacted-fault.xdmf ]; then
+  $script_dir/dynworkflow/compute_gof_fault_slip.py $output_dir/dyn_ $output_dir/dyn-kinmod_compacted-fault.xdmf
+else
+  $script_dir/dynworkflow/compute_gof_fault_slip.py $output_dir/dyn_ $output_dir/dyn-kinmod_extracted-fault.xdmf
+fi
+
+$script_dir/dynworkflow/compute_percentage_supershear.py $output_dir/dyn_ yaml_files/material.yaml
+
+if [ -f offsets.csv ]; then
+  $script_dir/dynworkflow/compare_offset.py $output_dir/dyn_ offsets.csv
+fi
+
+$script_dir/dynworkflow/add_source_files_to_waveform_config.py
+
+export OMP_NUM_THREADS=$(grep -c ^processor /proc/cpuinfo)
+
+if [ -f waveforms_config_regional.yaml ]; then
+  swf plot-waveforms waveforms_config_regional.yaml
+fi
+
+if [ -f waveforms_config_teleseismic.yaml ]; then
+  swf plot-waveforms waveforms_config_teleseismic.yaml
+fi
+
+$script_dir/dynworkflow/compile_scenario_macro_properties.py $output_dir
--- scripts/supermucNG1/generate_syn.sh
+++ scripts/supermucNG1/generate_syn.sh
@@ -30,33 +30,5 @@ source /etc/profile.d/modules.sh
 
 module load slurm_setup
 
-echo 'num_nodes:' $SLURM_JOB_NUM_NODES 'ntasks:' $SLURM_NTASKS
-ulimit -Ss 2097152
-
-export MP_SINGLE_THREAD=no
-unset KMP_AFFINITY
 export OMP_NUM_THREADS=48
-
-output_dir=extracted_output
-script_dir=../rapid-earthquake-dynamics/
-if [ -f $output_dir/dyn-kinmod_compacted-fault.xdmf ]; then
-    $script_dir/dynworkflow/compute_gof_fault_slip.py $output_dir/dyn_ $output_dir/dyn-kinmod_compacted-fault.xdmf
-else
-    #backwards compatibility
-    $script_dir/dynworkflow/compute_gof_fault_slip.py $output_dir/dyn_ $output_dir/dyn-kinmod_extracted-fault.xdmf
-fi
-
-$script_dir/dynworkflow/compute_percentage_supershear.py $output_dir/dyn_ yaml_files/material.yaml
-if [ -f offsets.csv ]; then
-    $script_dir/dynworkflow/compare_offset.py $output_dir/dyn_ offsets.csv
-fi
-
-$script_dir/dynworkflow/add_source_files_to_waveform_config.py
-export OMP_NUM_THREADS=$(grep -c ^processor /proc/cpuinfo)
-if [ -f waveforms_config_regional.yaml ]; then
-    swf plot-waveforms waveforms_config_regional.yaml
-fi
-if [ -f waveforms_config_teleseismic.yaml ]; then
-    swf plot-waveforms waveforms_config_teleseismic.yaml
-fi
-$script_dir/dynworkflow/compile_scenario_macro_properties.py $output_dir
+bash $(dirname "$0")/../common/generate_syn.sh
